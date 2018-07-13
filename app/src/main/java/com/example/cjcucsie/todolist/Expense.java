0fe4e9b1edca4488df2d7afb896c3cd379292857
First <first@example.com>
2018-07-13 17:04:20 +0800

    first

---
+++ app/src/main/java/com/example/cjcucsie/todolist/Expense.java
@@ -0,0 +1,45 @@
+package com.example.cjcucsie.todolist;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.arch.persistence.room.RoomDatabase;
+
+import java.util.List;
+
+import static com.example.cjcucsie.todolist.Expense.TABLE_EXPENSE;
+
+@Entity(tableName = TABLE_EXPENSE)
+public class Expense {
+    public static final String TABLE_EXPENSE = "expense";
+
+    public Expense()
+    {
+
+    }
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    @ColumnInfo(name = "list")
+    public String list;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void getId(int id)
+    {
+        this.id=id;
+    }
+    public String getList()
+    {
+        return list;
+    }
+
+    public void getList(String list)
+    {
+        this.list=list;
+    }
+}
